e3d702e1e14ba21763269a9c3fefd8b3af2b67a1
HaoYu Luo <haoyu.luo@thoughtworks.com>
2020-03-23 11:48:54 +0800

    fix double_decrease_quality_when_sellIn_less_than_zero

--- src/main/java/cn/xpbootcamp/gilded_rose/GildedRose.java
+++ src/main/java/cn/xpbootcamp/gilded_rose/GildedRose.java
@@ -1,14 +1,46 @@
 package cn.xpbootcamp.gilded_rose;
 
 public class GildedRose {
+    private static final int DEFAULT_QUALITY = 30;
     private int quality;
     private int SellIn;
 
-    public GildedRose(int sellIn) {
+    public static GildedRose buildGildedRose(int sellIn) {
+        GildedRose gildedRose = new GildedRose();
+        gildedRose.setSellIn(sellIn);
+        gildedRose.setQuality(getQualityBySellIn(sellIn));
+        return gildedRose;
+    }
+
+
+    public int getQuality() {
+        return quality;
+    }
+
+    public void setQuality(int quality) {
+        this.quality = quality;
+    }
+
+    public int getSellIn() {
+        return SellIn;
+    }
+
+    public void setSellIn(int sellIn) {
         SellIn = sellIn;
     }
 
-    public int getNowQuality() {
-        return (int) (Math.random() * 50);
+    private static int getQualityBySellIn(int sellIn) {
+
+        int nowQuality;
+        if (sellIn >= 0) {
+            nowQuality = DEFAULT_QUALITY + sellIn;
+        } else {
+            nowQuality = DEFAULT_QUALITY + 2 * sellIn;
+        }
+
+        if (nowQuality < 0) {
+            return 0;
+        }
+        return Math.min(nowQuality, 50);
     }
 }
--- src/test/java/cn/xpbootcamp/gilded_rose/GildedRoseTest.java
+++ src/test/java/cn/xpbootcamp/gilded_rose/GildedRoseTest.java
@@ -8,11 +8,23 @@ import org.junit.jupiter.api.Test;
 public class GildedRoseTest {
 
     @Test
-    public void should_quality_between_zero_and_fifty() {
-        GildedRose overdueGildedRose = new GildedRose(-1);
-        MatcherAssert.assertThat(overdueGildedRose.getNowQuality(), Matchers.greaterThan(0));
+    void should_quality_between_zero_and_fifty() {
+        GildedRose overdueGildedRose = GildedRose.buildGildedRose(-100);
+        MatcherAssert.assertThat(overdueGildedRose.getQuality(), Matchers.greaterThanOrEqualTo(0));
 
-        GildedRose normalGildedRose = new GildedRose(30);
-        MatcherAssert.assertThat(normalGildedRose.getNowQuality(), Matchers.lessThan(50));
+        GildedRose normalGildedRose = GildedRose.buildGildedRose(50);
+        MatcherAssert.assertThat(normalGildedRose.getQuality(), Matchers.lessThanOrEqualTo(50));
+    }
+
+    @Test
+    void should_double_decrease_quality_when_sellIn_less_than_zero() {
+        GildedRose normalGildedRose1 = GildedRose.buildGildedRose(1);
+        GildedRose normalGildedRose0 = GildedRose.buildGildedRose(0);
+        int normalDecrease = normalGildedRose1.getQuality() - normalGildedRose0.getQuality();
+        GildedRose overdueGildedRose1 = GildedRose.buildGildedRose(-1);
+        int overDueDecrease = normalGildedRose0.getQuality() - overdueGildedRose1.getQuality();
+        MatcherAssert.assertThat(overDueDecrease / normalDecrease, Matchers.equalTo(2));
+
+
     }
 }
